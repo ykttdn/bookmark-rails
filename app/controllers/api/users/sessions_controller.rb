2350b1413cde37d980a8d3ba35e139743804eefd
ykttdn <38461803+ykttdn@users.noreply.github.com>
2026-03-14 23:53:11 +0900

    Enable sign in

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -3,29 +3,17 @@
 module Api
   module Users
     class SessionsController < Devise::SessionsController
-      # before_action :configure_sign_in_params, only: [:create]
+      def create
+        user = User.find_by(email: params[:email])
 
-      # GET /resource/sign_in
-      # def new
-      #   super
-      # end
-
-      # POST /resource/sign_in
-      # def create
-      #   super
-      # end
-
-      # DELETE /resource/sign_out
-      # def destroy
-      #   super
-      # end
-
-      # protected
-
-      # If you have extra params to permit, append them to the sanitizer.
-      # def configure_sign_in_params
-      #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-      # end
+        if user&.valid_password?(params[:password])
+          sign_in(:user, user)
+          render status: :ok
+        else
+          render json: { errors: [I18n.t('devise.failure.not_found_in_database', authentication_keys: :email)] },
+                 status: :unauthorized
+        end
+      end
     end
   end
 end
